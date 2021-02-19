346643f94722c1a349c1d9e8716b693c16f5c0b9
Ednilton Junior <jrjunior_cg@live.com>
2021-02-19 16:36:38 -0300

    - Controlacao de autenticacao com rotas logadas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '9dcb457f2359ed3baf04ecfa4120b558',
+    expiresIn: '1d',
+  },
+};
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -3,13 +3,17 @@ import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 // parseIso converte uma string em date nativo do javascript
 // startOfHour recupera uma data com o inicio de uma determinada hora
 // isEqual verifica se duas datas estão iguais
 
 const appointmentsRouter = Router();
 
+appointmentsRouter.use(ensureAuthenticated);
+
 appointmentsRouter.get('/', async (request, response) => {
+  console.log(request.user);
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
 
