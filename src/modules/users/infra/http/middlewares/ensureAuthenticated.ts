1ab3a51ec5eec4b93b5d124611e125af75e239e0
Ednilton Junior <jrjunior_cg@live.com>
2021-02-21 03:02:03 -0300

    - Refatoraçao de codigo, passando as responsabilidades para modulos - Implementacao de controllers

--- src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
-import AppError from '../errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import authConfig from '../config/auth';
+import authConfig from '@config/auth';
 
 interface TokenPayload {
   iat: number;
@@ -31,7 +31,6 @@ export default function ensureAuthenticated(
       id: sub,
     };
 
-    console.log(decoded);
     return next();
   } catch (err) {
     throw new AppError('Invalid JWT token', 401);
