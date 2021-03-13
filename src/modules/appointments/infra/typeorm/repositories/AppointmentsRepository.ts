fe0d0f6f8c2e95094a9ccbc8d2ccfb4b0dc14c21
Ednilton Junior <jrjunior_cg@live.com>
2021-03-13 04:41:57 -0300

    Correcao formatacao da data

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -39,7 +39,6 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
         ),
       },
-      relations: ['user'],
     });
 
     return appointments;
@@ -63,6 +62,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -15,10 +15,10 @@ import '@shared/container';
 
 const app = express();
 
-app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 app.use(routes);
 
 app.use(errors());
@@ -31,6 +31,8 @@ app.use(
         message: err.message,
       });
     }
+
+    console.log(err);
 
     return response.status(500).json({
       status: 'error',
